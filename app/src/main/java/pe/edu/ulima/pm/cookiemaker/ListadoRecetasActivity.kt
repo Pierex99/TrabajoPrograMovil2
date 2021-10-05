774b68982cdada4b4893431b9e12a9240276c18b
Bruno <brunoandrechirinosmolina@gmail.com>
2021-10-05 00:08:02 -0500

    Paltalla principal de cookie maker

---
+++ app/src/main/java/pe/edu/ulima/pm/cookiemaker/ListadoRecetasActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.ulima.pm.cookiemaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoRecetasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_recetas)
+    }
+}
